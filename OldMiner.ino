722669380b027880a91f81bc7c878d68c5b49387
Jason Altekruse <altekrusejason@gmail.com>
2025-03-23 09:48:50 -0500

    WIP tyring to frankenstein the adafruit thing into arduboy game
    - this is part of a changeset, I got the "game" compiling by just commenting out all of the arduoy references
    - I was going to replace them with calls to adafruit, or my own button handling code
    - then I realized I could edit Old Miner less if I implemented the arduboy interface
    - I did get the header files incluuded in the project, but it barked about #DEFINES
    - I tried moving the import of arduboy later and it got rid of the errors, I have more to learn about C/C++... and library/namespace conflicts
    - this is a portion of an overall change, I'll save the other edits that comment out all of the arduboy stuff if I need to come back to it
    - but now I'm kind of hoping I can just let adafruit and arduboy live together if I import arduboy second..

--- OldMiner.ino
+++ OldMiner.ino
@@ -12,6 +12,52 @@ version 2.1 of the License, or (at your option) any later version.
 #include <Tinyfont.h>//830 PROGMEM - 28 RAM
 #include "sprites.h"
 
+
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
+#include "arduboy_mod/Arduboy2_mod.h"
+
+/* Uncomment the initialize the I2C address , uncomment only one, If you get a totally blank screen try the other*/
+#define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
+//#define i2c_Address 0x3d //initialize with the I2C addr 0x3D Typically Adafruit OLED's
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1   //   QT-PY / XIAO
+Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000
+};
+
 // make an instance of arduboy used for many functions
 Arduboy2 arduboy;
 
@@ -202,6 +248,8 @@ entity entities[NUM_ENTITIES] = {
 void setup() {
   // initiate arduboy instance
   arduboy.begin();
+  delay(1000);
+  display.begin(i2c_Address, true);
 
   // TODO - this isn't working for me
   // Serial.begin(9600);
